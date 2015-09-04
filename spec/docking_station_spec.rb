166a38e45ebb16c829c39a2a66fd8ac09761e7eb
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-04 18:37:43 +0100

    Cleaned up docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,16 @@
 require 'dockingstation'
 
 describe DockingStation do
-  
+  it { is_expected.to respond_to(:release_bike) }
+
+  it "releases working bikes" do
+    subject.dock Bike.new
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       bike = Bike.new 
@@ -9,16 +18,6 @@ describe DockingStation do
       subject.dock bike
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
-
-    it { is_expected.to respond_to(:release_bike) }
-
-    it "releases working bikes" do
-      subject.dock Bike.new
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
-
-    it { is_expected.to respond_to(:dock).with(1).argument }
   end
 
   describe '#dock' do
@@ -26,14 +25,5 @@ describe DockingStation do
   		subject.capacity.times { subject.dock(Bike.new)}
   		expect { subject.dock(Bike.new).to raise_error 'Full'}
   	end
-  
-    # it "has a changable capacity" do
-    # docking_station = DockingStation.new(10)
-    # expect(docking_station.capacity).to eq(10)
-    # end
-
-    # it "has a default capacity" do
-    # docking_station = DockingStation.new
-    # expect(docking_station.capacity).to eq(20)
   end
 end
